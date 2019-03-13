5ebaaaad0de553b898bc0d71e78a458dbc9f57d1
Patrick Liu <zeldalink0515@gmail.com>
2019-03-13 20:46:27 +0800

    Make jest resolving monorepo pkgs in source

--- packages/machinat-utility/src/index.js
+++ packages/machinat-utility/src/index.js
@@ -3,7 +3,6 @@ export { default as isValidElementType } from './isValidElementType';
 export { default as formatElement } from './formatElement';
 export { default as joinTextValues } from './joinTextValues';
 export { default as valuesOfAssertedType } from './valuesOfAssertedType';
-export { default as applyPlugins } from './applyPlugins';
 export { default as mixin } from './mixin';
 export { default as toJSONWithProto } from './toJSONWithProto';
 
