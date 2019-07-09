0f542ec43736256f4eb5d2e0822369d2ca1ccaad
Patrick Liu <zeldalink0515@gmail.com>
2019-07-09 15:01:17 +0800

    Export router and matchers of http-adaptor

--- packages/machinat-http-adaptor/src/index.js
+++ packages/machinat-http-adaptor/src/index.js
@@ -1,3 +1,6 @@
+// @flow
+export { default as Router } from './router';
 export { default as connectRequest } from './connectRequest';
 export { default as connectUpgrade } from './connectUpgrade';
 export { default as connectKoa } from './connectKoa';
+export * from './requestMatcher';
