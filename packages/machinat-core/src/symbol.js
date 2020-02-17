cfc3fc0d5e23494722a25cbaff6c95e1a8e46feb
Patrick Liu <zeldalink0515@gmail.com>
2020-02-17 18:19:20 +0800

    Rename package machinat to @machinat/core

---
+++ packages/machinat-core/src/symbol.js
@@ -0,0 +1,28 @@
+// @flow
+export const MACHINAT_ELEMENT_TYPE = Symbol('machinat.element');
+
+export const MACHINAT_FRAGMENT_TYPE = Symbol('machinat.element.fragment');
+
+export const MACHINAT_PAUSE_TYPE = Symbol('machinat.element.pause');
+
+export const MACHINAT_PROVIDER_TYPE = Symbol('machinat.element.provider');
+
+export const MACHINAT_THUNK_TYPE = Symbol('machinat.element.thunk');
+
+export const MACHINAT_RAW_TYPE = Symbol('machinat.element.raw');
+
+export const MACHINAT_NATIVE_TYPE = Symbol('machinat.component.native');
+
+export const MACHINAT_SERVICES_CONTAINER = Symbol(
+  'machinat.services.container'
+);
+
+export const MACHINAT_SERVICES_INTERFACEABLE = Symbol(
+  'machinat.services.interfaceable'
+);
+
+export const MACHINAT_SERVICES_PROVIDER = Symbol('machinat.services.provider');
+
+export const MACHINAT_SERVICES_ABSTRACTION = Symbol(
+  'machinat.services.abstraction'
+);
