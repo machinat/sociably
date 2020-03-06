0e3a91a5b245187a9149b32ca074e125aecf1d06
Patrick Liu <zeldalink0515@gmail.com>
2020-03-06 14:03:21 +0800

    Add jest-snapshot-serielizer

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
     __DEV__: true,
   },
   moduleNameMapper: {
-    '^(machinat-?[^/]*)$': '<rootDir>/packages/$1/src',
+    '^@machinat/([^/]*)(.*)$': '<rootDir>/packages/machinat-$1/src$2',
   },
   setupFiles: ['<rootDir>/node_modules/moxy/lib/extends/jest.js'],
+  snapshotSerializers: ['@machinat/jest-snapshot-serielizer'],
 };
