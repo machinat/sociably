e46d76b744f990dc9e55bdaafb46997891402109
Patrick Liu <zeldalink0515@gmail.com>
2021-05-12 20:13:03 +0800

    Bye partition

--- packages/stream/src/index.ts
+++ packages/stream/src/index.ts
@@ -1,7 +1,6 @@
 export { default as Stream } from './stream';
 export { default as fromApp } from './fromApp';
 export { default as merge } from './merge';
-export { default as partition } from './partition';
 export { default as conditions } from './conditions';
 export { default as pipe } from './pipe';
 export { STREAMING_KEY_I } from './interface';
