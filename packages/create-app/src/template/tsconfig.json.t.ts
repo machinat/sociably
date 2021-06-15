1c4ae61d9d2ef7697a4289d292fcb62715188ace
Patrick Liu <zeldalink0515@gmail.com>
2021-06-15 15:58:58 +0800

    Key order in tsconfig template

--- packages/create-app/src/template/tsconfig.json.t.ts
+++ packages/create-app/src/template/tsconfig.json.t.ts
@@ -6,23 +6,22 @@ export default () =>
   "include": ["./src/**/*"],
   "exclude": ["./src/webview/**/*"],
   "compilerOptions": {
+    "rootDir": "./src",
     "outDir": "./lib",
     "lib": ["es2018"],
+    "types": ["node"],
     "module": "commonjs",
     "target": "es2018",
-    "strictNullChecks": true,
-    "esModuleInterop": true,
     "jsx": "react",
     "jsxFactory": "Machinat.createElement",
     "jsxFragmentFactory": "Machinat.Fragment",
+    "strictNullChecks": true,
+    "esModuleInterop": true,
     "skipLibCheck": true,
     "noUnusedLocals": true,
     "composite": true,
     "declaration": true,
-    "sourceMap": true,
-    "types": ["node"],
-    "rootDir": "./src",
-    "outDir": "./lib"
+    "sourceMap": true
   }
 }
 `);
