19197099dce005dfecf580b5a937ea665dec2c92
Patrick Liu <zeldalink0515@gmail.com>
2021-12-23 22:41:39 +0800

    feat: remove webview/noneAuthenticator export

--- packages/webview/src/client/index.ts
+++ packages/webview/src/client/index.ts
@@ -1,3 +1,9 @@
 export { default } from './client';
 export { default as useEventReducer } from './useEventReducer';
 export * from './types';
+
+export {
+  NoneClientAuthenticator,
+  NoneUser,
+  NoneChannel,
+} from '../noneAuthenticator';
--- packages/webview/src/index.ts
+++ packages/webview/src/index.ts
@@ -3,3 +3,9 @@ export { WebviewBot } from './bot';
 export { WebviewReceiver } from './receiver';
 export { Event } from './component';
 export * from './types';
+
+export {
+  default as NoneAuthenticator,
+  NoneUser,
+  NoneChannel,
+} from './noneAuthenticator';
