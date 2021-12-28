b5fcc5f690414c5b3db214435ec63af4de1255ab
Patrick Liu <zeldalink0515@gmail.com>
2021-12-28 17:00:48 +0800

    feat: merge multiple stream

--- packages/stream/src/__tests__/merge.spec.ts
+++ packages/stream/src/__tests__/merge.spec.ts
@@ -4,63 +4,80 @@ import Stream from '../stream';
 import merge from '../merge';
 import { STREAMING_KEY_I } from '../interface';
 
-it('merge events form two streams', () => {
+it('merge events form  streams', () => {
   const sourceA = new Stream();
   const sourceB = new Stream();
+  const sourceC = new Stream();
   const eventListener = moxy();
   const eventContainer = moxy(
     makeContainer({ deps: [STREAMING_KEY_I] })(() => eventListener)
   );
 
-  const destination = merge(sourceA, sourceB);
+  const destination = merge(sourceA, sourceB, sourceC);
   destination.subscribe(eventContainer);
 
-  sourceA.next({ scope: createEmptyScope(), value: 'FOO', key: 'foo.channel' });
+  sourceA.next({ scope: createEmptyScope(), value: 'FOOA', key: 'foo.chan' });
   expect(eventListener.mock).toHaveBeenCalledTimes(1);
-  expect(eventListener.mock).toHaveBeenNthCalledWith(1, 'FOO');
-  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('foo.channel');
+  expect(eventListener.mock).toHaveBeenNthCalledWith(1, 'FOOA');
+  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('foo.chan');
 
-  sourceB.next({ scope: createEmptyScope(), value: 'FOO', key: 'foo.channel' });
+  sourceB.next({ scope: createEmptyScope(), value: 'FOOB', key: 'foo.chan' });
   expect(eventListener.mock).toHaveBeenCalledTimes(2);
-  expect(eventListener.mock).toHaveBeenNthCalledWith(2, 'FOO');
+  expect(eventListener.mock).toHaveBeenNthCalledWith(2, 'FOOB');
+  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('foo.chan');
 
-  sourceB.next({ scope: createEmptyScope(), value: 'BAR', key: 'bar.channel' });
+  sourceC.next({ scope: createEmptyScope(), value: 'FOOC', key: undefined });
   expect(eventListener.mock).toHaveBeenCalledTimes(3);
-  expect(eventListener.mock).toHaveBeenNthCalledWith(3, 'BAR');
-  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('bar.channel');
+  expect(eventListener.mock).toHaveBeenNthCalledWith(3, 'FOOC');
+  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith(undefined);
 
-  sourceA.next({ scope: createEmptyScope(), value: 'BAZ', key: 'baz.channel' });
+  sourceB.next({ scope: createEmptyScope(), value: 'BARB', key: 'bar.chan' });
   expect(eventListener.mock).toHaveBeenCalledTimes(4);
-  expect(eventListener.mock).toHaveBeenNthCalledWith(4, 'BAZ');
-  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('baz.channel');
+  expect(eventListener.mock).toHaveBeenNthCalledWith(4, 'BARB');
+  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('bar.chan');
+
+  sourceA.next({ scope: createEmptyScope(), value: 'BAZA', key: 'baz.chan' });
+  expect(eventListener.mock).toHaveBeenCalledTimes(5);
+  expect(eventListener.mock).toHaveBeenNthCalledWith(5, 'BAZA');
+  expect(eventContainer.$$factory.mock).toHaveBeenCalledWith('baz.chan');
 });
 
-it('merge errors form two streams', () => {
+it('merge errors form streams', () => {
   const sourceA = new Stream();
   const sourceB = new Stream();
+  const sourceC = new Stream();
   const errorListener = moxy();
   const errorContainer = moxy(
     makeContainer({ deps: [STREAMING_KEY_I] })(() => errorListener)
   );
 
-  const destination = merge(sourceA, sourceB);
+  const destination = merge(sourceA, sourceB, sourceC);
   destination.catch(errorContainer);
 
   sourceA.error({
     scope: createEmptyScope(),
     value: new Error('boo'),
-    key: 'foo.channel',
+    key: 'foo.chan',
   });
   expect(errorListener.mock).toHaveBeenCalledTimes(1);
   expect(errorListener.mock).toHaveBeenNthCalledWith(1, new Error('boo'));
-  expect(errorContainer.$$factory.mock).toHaveBeenCalledWith('foo.channel');
+  expect(errorContainer.$$factory.mock).toHaveBeenCalledWith('foo.chan');
 
   sourceB.error({
     scope: createEmptyScope(),
     value: new Error('beer'),
-    key: 'bar.channel',
+    key: 'bar.chan',
   });
   expect(errorListener.mock).toHaveBeenCalledTimes(2);
   expect(errorListener.mock).toHaveBeenNthCalledWith(2, new Error('beer'));
-  expect(errorContainer.$$factory.mock).toHaveBeenCalledWith('bar.channel');
+  expect(errorContainer.$$factory.mock).toHaveBeenCalledWith('bar.chan');
+
+  sourceC.error({
+    scope: createEmptyScope(),
+    value: new Error('bbq'),
+    key: undefined,
+  });
+  expect(errorListener.mock).toHaveBeenCalledTimes(3);
+  expect(errorListener.mock).toHaveBeenNthCalledWith(3, new Error('bbq'));
+  expect(errorContainer.$$factory.mock).toHaveBeenCalledWith(undefined);
 });
--- packages/stream/src/merge.ts
+++ packages/stream/src/merge.ts
@@ -1,14 +1,15 @@
 import Stream from './stream';
 import { StreamingFrame } from './types';
 
-const merge = <T>(sourceA: Stream<T>, sourceB: Stream<T>): Stream<T> => {
+const merge = <T>(...sources: Stream<T>[]): Stream<T> => {
   const destination = new Stream<T>();
 
   const next = (frame: StreamingFrame<T>) => destination.next(frame);
   const error = (frame: StreamingFrame<Error>) => destination.error(frame);
 
-  sourceA._subscribe(next, error);
-  sourceB._subscribe(next, error);
+  for (const source of sources) {
+    source._subscribe(next, error);
+  }
 
   return destination;
 };
