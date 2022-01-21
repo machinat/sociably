7e2e274493ecd297a04448d34bb017522258ec2e
Patrick Liu <zeldalink0515@gmail.com>
2022-01-21 17:12:25 +0800

    feat: build with typescript

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ module.exports = {
     }],
     ['@babel/preset-env', {
         targets: { node: 10 },
+        loose: true,
     }],
     '@machinat/babel-preset',
   ],
--- package.mk
+++ package.mk
@@ -1,44 +1,32 @@
 source_files := $(shell find $(CURDIR)/src -regex '.*\.\tsx?' -not -regex '.*/__[^/]*__/.*')
 lib_files := $(addsuffix .js, $(basename $(patsubst $(CURDIR)/src/%, lib/%, $(source_files))))
-babel_conifg := $(CURDIR)/../../babel.config.js
-babel := $(CURDIR)/../../node_modules/.bin/babel
 tsc := $(CURDIR)/../../node_modules/.bin/tsc
 polyfill_exports := $(CURDIR)/../../node_modules/.bin/polyfill-exports
 
 .PHONY: all build clean
 
-all: $(lib_files) build tsconfig.tsbuildinfo polyfill-exports.js
+all: $(lib_files) build polyfill-exports.js
 
 lib/%.js: src/%.ts*
 	touch $(CURDIR)/.mark_require_building
 
 build: | lib
 	if [ -f $(CURDIR)/.mark_require_building ]; then \
-	  NODE_ENV=production $(babel) src \
-	    -d lib \
-	    --config-file $(babel_conifg) \
-	    --extensions .ts,.tsx \
-	    --source-maps \
-	    --verbose; \
-	  rm $(CURDIR)/.mark_require_building; \
+	  $(tsc) \
+	    --build \
+	    --listEmittedFiles \
+	    $(CURDIR)/tsconfig.build.json; \
+	    rm $(CURDIR)/.mark_require_building; \
 	fi
 
-prepack: clean lib tsconfig.tsbuildinfo polyfill-exports.js
-	 NODE_ENV=production $(babel) src \
-	  -d lib \
-	  --config-file $(babel_conifg) \
-	  --extensions .ts,.tsx \
-	  --source-maps \
-	  --verbose; \
-
-lib:
-	mkdir lib
-
-tsconfig.tsbuildinfo: $(source_files) lib
+prepack: clean lib polyfill-exports.js
 	$(tsc) \
 	  --build \
 	  --listEmittedFiles \
-	  $(CURDIR)/tsconfig.build.json
+	  $(CURDIR)/tsconfig.build.json;
+
+lib:
+	mkdir lib
 
 polyfill-exports.js: $(CURDIR)/package.json
 	$(polyfill_exports) $(CURDIR) --ts-declaration
