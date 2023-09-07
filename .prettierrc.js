34b6465e409c735148737ab72dc0a8bd3f01d92e
Patrick Liu <patrick.liu@inline.tw>
2023-09-07 18:22:15 +0800

    chore: fix prettier-2 for jest inline snapshot

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   singleQuote: true,
-  parser: 'typescript',
   plugins:
-    // HACK: prettier-plugin-jsdoc doesn't work with prettier@2 which used by jest
-    //       don't use it when testing
+    // HACK: prettier-plugin-jsdoc doesn't work with prettier@2 which used by jest.
+    //       Don't use it while testing. Also the plugin name is modified to avoid
+    //       plugin auto searching feature of prettier@2.
     process.env.JEST_WORKER_ID === undefined && process.env.NODE_ENV !== 'test'
-      ? ['prettier-plugin-jsdoc']
+      ? ['prettier-3-plugin-jsdoc']
       : [],
-  pluginSearchDirs: false,
   tsdoc: true,
 };
