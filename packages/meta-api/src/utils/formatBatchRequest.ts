93d07877947eb0edc202c4b64adb6c0f914a1a1e
Patrick Liu <patrick.liu@inline.tw>
2023-09-22 11:17:25 +0800

    fix: fix meta api job url with query

--- packages/meta-api/src/utils/formatBatchRequest.ts
+++ packages/meta-api/src/utils/formatBatchRequest.ts
@@ -1,25 +1,33 @@
+import { parse as parseUrl, format as formatUrl } from 'url';
+import { encode as encodeQueryString } from 'querystring';
 import { MetaBatchRequest, MetaApiJobRequest } from '../types.js';
 
-const appendField = (body: string, key: string, value: string) =>
-  `${body.length === 0 ? '' : `${body}&`}${key}=${encodeURIComponent(value)}`;
-
-const encodeParams = (fields: Record<string, unknown>): string => {
-  let body = '';
-
-  for (const key of Object.keys(fields)) {
-    const fieldValue = fields[key];
-
-    if (fieldValue !== undefined) {
-      body = appendField(
-        body,
+const formatQueryParams = (queryParams: Record<string, unknown>) =>
+  Object.fromEntries(
+    Object.entries(queryParams)
+      .filter(([, value]) => value !== undefined)
+      .map(([key, value]) => [
         key,
-        typeof fieldValue === 'string'
-          ? fieldValue
-          : JSON.stringify(fieldValue),
-      );
-    }
-  }
-  return body;
+        typeof value === 'string' || typeof value === 'number'
+          ? value.toString()
+          : JSON.stringify(value),
+      ]),
+  );
+
+const formatUrlWithQuery = (
+  relativeUrl: string,
+  queryParams: Record<string, unknown>,
+): string => {
+  const parsedUrl = parseUrl(relativeUrl, true);
+  return formatUrl({
+    protocol: parsedUrl.protocol,
+    hostname: parsedUrl.hostname,
+    pathname: parsedUrl.pathname,
+    query: {
+      ...parsedUrl.query,
+      ...formatQueryParams(queryParams),
+    },
+  });
 };
 
 const formatRequestParams = (
@@ -30,20 +38,20 @@ const formatRequestParams = (
   method === 'GET' || method === 'DELETE'
     ? {
         method,
-        relative_url: `${url}?${encodeParams({
+        relative_url: formatUrlWithQuery(url, {
           ...params,
           access_token: accessToken,
           appsecret_proof: appSecretProof,
-        })}`,
+        }),
         body: undefined,
       }
     : {
         method,
-        relative_url: `${url}?${encodeParams({
+        relative_url: formatUrlWithQuery(url, {
           access_token: accessToken,
           appsecret_proof: appSecretProof,
-        })}`,
-        body: params ? encodeParams(params) : undefined,
+        }),
+        body: params ? encodeQueryString(formatQueryParams(params)) : undefined,
       };
 
 export default formatRequestParams;
